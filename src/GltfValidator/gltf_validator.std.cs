d2270ec6776f7a89fa58c6c2b47cc58728f8e8d4
vpenades <5433822+vpenades@users.noreply.github.com>
2021-12-18 12:44:43 +0100

    updated target platforms

--- src/GltfValidator/gltf_validator.std.cs
+++ src/GltfValidator/gltf_validator.std.cs
@@ -14,17 +14,22 @@ namespace GltfValidator
 
         public static ValidationReport ValidateFile(string gltfFilePath, int timeOut = 10000)
         {
-            throw new NotSupportedException();
+            return _ThrowMessage();
         }
 
         public static async Task<ValidationReport> ValidateFileAsync(string gltfFilePath, System.Threading.CancellationToken token)
         {
-            throw new NotSupportedException();
+            return await Task.FromResult(_ThrowMessage());
         }
 
         public static async Task<ValidationReport> ValidateFileAsyncProcessX(string gltfFilePath, System.Threading.CancellationToken token)
         {
-            throw new NotSupportedException();
+            return await Task.FromResult(_ThrowMessage());
+        }
+
+        private static ValidationReport _ThrowMessage()
+        {
+            throw new NotSupportedException("Supported platforms: net5.0 and net6.0 on Windows");
         }
     }
 }
